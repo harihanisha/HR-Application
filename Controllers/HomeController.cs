a9b1697e5c2447381a5f6fe8d9d0a8f8e87afe29
Hellen Harry <59941047+harihanisha@users.noreply.github.com>
2020-01-21 13:38:47 -0500

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace HellenHarryAssigment3.Controllers
+namespace HRApplication.Controllers
 {
     public class HomeController : Controller
     {
